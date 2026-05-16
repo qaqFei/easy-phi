623bd17c0fb797ef0aee08a7980a05ce72bab245
qaqFei <qaq_fei@163.com>
2026-05-16 13:58:47 +0800

    feat: enable MSAA 4x for window

--- test_files/test_shared.hpp
+++ test_files/test_shared.hpp
@@ -1107,6 +1107,7 @@ struct WindowWOSkia {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_SAMPLES, 4);
         if (hidden) glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
         auto* vm = (GLFWvidmode*)glfwGetVideoMode(glfwGetPrimaryMonitor());
